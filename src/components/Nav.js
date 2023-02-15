5bd408fa32b5ffb3e50c07fbb5d717967cb18b85
Robert Smith <56047287+RobSmithDevelopment@users.noreply.github.com>
2023-02-15 15:30:38 +0000

    Made hamburger image show toggle appearance of nav links on click

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import logo from '../images/Logo.webp';
 import menu from '../images/menu.png';
 
 const Nav = () => {
-
-    
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+      
+    function handleMenuClick() {
+        setIsMenuOpen(!isMenuOpen);
+        console.log("hello!");
+    }
 
     return (
       <nav className="nav-container">
             <img className="nav-logo" src={logo}></img>
 
-            <ul>
+            <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
                 <li className="nav-li"><Link to="/">Home</Link></li>
                 <li className="nav-li"><Link to="/about">About Me</Link></li>
                 <li className="nav-li"><Link to="/work">My Work</Link></li>
                 <li className="nav-li"><Link to="/contact">Contact Me</Link></li>
             </ul>
 
-            <img className="nav-menu" src={menu}></img>
+            <img className="nav-menu" src={menu} onClick={handleMenuClick}></img>
         </nav>
     )
 }
+
 
 export default Nav;
