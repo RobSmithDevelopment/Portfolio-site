c710acc69d03caa79d8d03ae5006e08ab0bef786
Robert Smith <56047287+RobSmithDevelopment@users.noreply.github.com>
2023-02-15 15:00:27 +0000

    Added a hamburger menu on 600px or less screen size, removing the links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import logo from '../images/Logo.webp'
+import logo from '../images/Logo.webp';
+import menu from '../images/menu.png';
 
 const Nav = () => {
+
+    
+
     return (
       <nav className="nav-container">
             <img className="nav-logo" src={logo}></img>
 
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About Me</Link></li>
-                <li><Link to="/work">My Work</Link></li>
-                <li><Link to="/contact">Contact Me</Link></li>
+                <li className="nav-li"><Link to="/">Home</Link></li>
+                <li className="nav-li"><Link to="/about">About Me</Link></li>
+                <li className="nav-li"><Link to="/work">My Work</Link></li>
+                <li className="nav-li"><Link to="/contact">Contact Me</Link></li>
             </ul>
+
+            <img className="nav-menu" src={menu}></img>
         </nav>
     )
 }
