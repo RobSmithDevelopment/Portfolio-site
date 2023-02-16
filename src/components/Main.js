12f02f3e6b541051c113fd5ded3b5e8ecb72737e
Robert Smith <56047287+RobSmithDevelopment@users.noreply.github.com>
2023-02-16 16:18:52 +0000

    Added icon to button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from "react";
-import me from '../images/Myself.png'
+import { Link } from 'react-router-dom';
+import me from '../images/Myself.png';
+import arrow from '../images/right-arrow.png';
 
 const Main = () => {
     return (
@@ -9,7 +11,7 @@ const Main = () => {
             <div className="main-text">
                 <h1 className="main-text-h1">Hi, I'm Robert Smith!</h1>
                 <h2 className="main-text-h2">Junior Frontend Developer</h2>
-                <button className="main-text-button">See My Projects</button>
+                <button className="main-text-button"><Link to="/work" className="main-text-button">See My Projects <img src={arrow} className="main-arrow"></img></Link></button>
             </div>
         </div>
     )
