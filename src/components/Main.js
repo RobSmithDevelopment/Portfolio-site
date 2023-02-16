144c1d6c0c8f2b859138fa6bda9da4998b32c32b
Robert Smith <56047287+RobSmithDevelopment@users.noreply.github.com>
2023-02-16 15:52:26 +0000

    Added text and some styling to main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,11 @@ const Main = () => {
         <div className="main-container">
             <img className="main-image" src={me}></img>
             <div className="vl"></div>
-            <p>Hi I'm Rob</p>
+            <div className="main-text">
+                <h1 className="main-text-h1">Hi, I'm Robert Smith!</h1>
+                <h2 className="main-text-h2">Junior Frontend Developer</h2>
+                <button className="main-text-button">See My Projects</button>
+            </div>
         </div>
     )
 }
