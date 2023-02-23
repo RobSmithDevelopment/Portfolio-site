97c612ed4a8b0b3cfd1fdc5bc84d0f90e94abfb0
Robert Smith <56047287+RobSmithDevelopment@users.noreply.github.com>
2023-02-23 18:47:52 +0000

    Began creation of about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,26 @@
 import React from "react";
+import HTML from '../images/HTML.png';
+import CSS from '../images/CSS.png';
+import JS from '../images/Javascript.png';
+import REACT from '../images/React.png';
+import Node from '../images/Node.png';
+import Sass from '../images/Sass.png';
 
 const About = () => {
     return (
-       <h1> About </h1>
+       <div className="about-container">
+            <div className="about-icons">
+                <img src={HTML}></img>
+                <img src={CSS}></img>
+                <img src={JS}></img>
+                <img src={REACT}></img>
+                <img src={Node}></img>
+                <img src={Sass}></img>
+            </div> 
+            <div className="about-text">
+
+            </div>
+       </div>
     )
 }
 
