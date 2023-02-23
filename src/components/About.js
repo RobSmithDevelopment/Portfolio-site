17c45d4c9b2951f77c7c944b26d2586e90ffab22
Robert Smith <56047287+RobSmithDevelopment@users.noreply.github.com>
2023-02-23 23:27:14 +0000

    Updated text on about section

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ import Sass from '../images/Sass.png';
 const About = () => {
     return (
        <div className="about-container">
-        
+        {/* icons supplied by icons8.com */}
         <div className="about-icons">
             <div>
                 <img src={HTML}></img>
@@ -39,8 +39,8 @@ const About = () => {
 
         <div className="about-text">
             <p>Hi there! My name is Robert Smith, and I am a front-end developer. My passion for web development began when a friend offered to show me some basic coding. As I learned more about the field, I realized that front-end development perfectly blends my love for visual creativity with my logical and analytical thinking.
-            After self-teaching for a short while, I decided to pursue a more formal education in web development. I enrolled in a scholarship Department of Education web development bootcamp, where I honed my skills in HTML, CSS, and JavaScript. I graduated from the program with an average grade of 100%.
-            Now, I am eager to embark on my first professional opportunity in front-end development. I am excited to bring my creativity and technical expertise to a team that values innovation, collaboration, and excellence. Let's build beautiful and functional websites together!</p> 
+            After self-teaching for a short while, I decided to pursue a more formal education in web development. I enrolled in a scholarship Department of Education web development bootcamp, where I honed my skills in HTML, CSS, JavaScript, NodeJS and React. I graduated from the program with an average grade of 100%.
+            Now, I am eager to embark on my first professional opportunity in front-end development. I am excited to bring my creativity, technical knowledge and passion for learning and personal growth to a team that values collaboration, hard work and fun! Let's build beautiful and functional websites together!</p> 
         </div>
 
        </div>
